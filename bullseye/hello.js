37bd5335c080d14a39c2452da4dbc973f3245411
ND02 <97234943+ND02@users.noreply.github.com>
2022-01-27 20:24:03 -0800

    Update hello.js

--- bullseye/hello.js
+++ bullseye/hello.js
@@ -1,4 +1,5 @@
-var table = prompt("Please enter value:");             // Unit of table
+var prompt= prompt("Please enter value:");
+var table = prompt(); // Unit of table
 var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplcation Table</h2>';              // Message
@@ -6,16 +7,16 @@ var el = document.getElementById('blackboard');
 el.innerHTML = msg;
 
 if (operator === 'multiplication') {
-   Do multiplication
+   //Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-} else {
-  Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
+//else {
+ // Do addition
+  //while (i < 11) {
+   // msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+   // i++;
   }
 }
 
