19a17da414e0d9edc2ed3bfcc68639dd16647aec
ND02 <97234943+ND02@users.noreply.github.com>
2022-01-29 17:25:49 -0800

    Update contact.js

--- portfolio/contact.js
+++ portfolio/contact.js
@@ -1,9 +1,3 @@
-info="<form action=&#34;/thankyou.html&#34;>
-  			<label for=&#34;fname&#34;>First name:</label><br>
-  			<input type=&#34;text&#34; id=&#34;fname&#34; name=&#34;fname&#34; value=&#34;John&#34;><br>
-  			<label for=&#34;lname&#34;>Last name:</label><br>
- 			 <input type=&#34;text&#34; id=&#34;lname&#34; name=&#34;lname&#34; value=&#34;Doe&#34;><br><br> 
-			<a href=&#34;thankyou.html&#34;>
-			<input type=&#34;submit&#34;/>"
-
-document.write(info)
+var name = prompt("Please enter you full name:");
+var email = Enter your email address:
+var comments = Any additional comments:
