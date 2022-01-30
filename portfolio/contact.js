3d8b6576e0adb12f2f3c6122600f159b96d5b19a
ND02 <97234943+ND02@users.noreply.github.com>
2022-01-29 16:33:47 -0800

    Update contact.js

--- portfolio/contact.js
+++ portfolio/contact.js
@@ -1,3 +1,3 @@
-var name= prompt("Please enter your first and last name:", Will Rutledge);
-var email= Enter email:
-var msg= Enter your message and any additional information:
+var name = prompt("Please enter your first and last name:", Will Rutledge);
+var email = Enter email:
+var msg = Enter your message and any additional information:
