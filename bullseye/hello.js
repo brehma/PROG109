4b0f34340df7e4ab6edabe75c2df889b749036fd
ND02 <97234943+ND02@users.noreply.github.com>
2022-01-29 19:55:56 -0800

    Update hello.js

--- bullseye/hello.js
+++ bullseye/hello.js
@@ -1,12 +1,20 @@
-// take input from the user
-const number = parseInt(prompt('Enter an integer: '));
+var table = prompt("Please enter your value:");             // Unit of table
+var operator = 'multiplication'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';              // Message
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
 
-//creating a multiplication table
-for(let i = 1; i <= 10; i++) {
-
-    // multiply i with number
-    const result = i * number;
-
-    // display the result
-    console.log(`${number} * ${i} = ${result}`);
+if (operator === 'multiplication') {
+  // Do addition
+  while (i < 11) {
+    msg += i * ' x ' + table + ' = ' + (i + table) + '<br />';
+    i*;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
 }
