70162ed62509a591ae447846fbce80c05ae555ea
ND02 <97234943+ND02@users.noreply.github.com>
2022-02-09 11:03:38 -0800

    section tags, username and password sections

--- portfolio/contactme.js
+++ portfolio/contactme.js
@@ -17,7 +17,7 @@
     if (lastname==="null" || lastname==="" || lastname.length > 50)
         errorMessages += "<p>The lastname is required and cannot be greater than 50 characters</p>";
     else
-       validFirstname = true;
+       validLastname = true;
        document.getElementById("errorMessages").innerHTML = errorMessages;
        return (validFirstname && validLastname);		
  				 
@@ -30,22 +30,40 @@
      else
        validEmail = true;
        document.getElementById("errorMessages").innerHTML = errorMessages;
-        return (validEmail);  
+     return (validEmail);  
 	
      //Phone Number
      var phone = document.getElementById("phone").value;
 	if (isNaN(phone) || phone.lenght >15 || phone===null || phone==="")
 	   errorMessages += "<p>Invalid Phone Number</p>";}
 	else
-	validPhone = true;
-        document.getElementById("errorMessages").innerHTML = errorMessages;
-        return (validPhone);  
-	  
-	//You can also do pattern matching by using the match() method from the string object
-	var numbers=/^[0-9]+$/;
-	if (phone===null||phone ==="" ||phone.length>15|| !phone.match(numbers))
-	//...
+		validPhone = true;
+		document.getElementById("errorMessages").innerHTML = errorMessages;
+	return (validPhone);  
+ 
+     //Username
+     var username = document.getElementById("username").value;
+	if (username==="null" || username==="" || username.length > 12)
+	    errorMessages += "<p>The username is required and cannot be greater than 12 characters</p>";
 	else
-	//...
-  }
+	       validUsername = true;
+	       document.getElementById("errorMessages").innerHTML = errorMessages;
+	return (validUsername);
+
+     //Password
+     var password = document.getElementById("password").value;
+	if (firstname==="null" || firstname==="" || firstname.length > 20)
+            errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
+        else
+	       validPassword = true;
+	       document.getElementById("errorMessages").innerHTML = errorMessages;
+        return (validUsername && validPassword);
+  
+    //Address
+	
+    //City
+   //State
+  //Country
+ //Zipcode
+}
 </form>
