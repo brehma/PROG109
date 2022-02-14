776f134e5e7175a7a7aed8e829ee9da40fbbda51
ND02 <97234943+ND02@users.noreply.github.com>
2022-02-14 14:33:26 -0800

    Create rhombus.js

---
+++ rhombus/rhombus.js
@@ -0,0 +1,45 @@
+// var rHeight = 5;
+// var colorEven = "orange";
+// var colorOdd = "black";
+// var symbol = "*";
+
+function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol){
+  upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+  downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+}
+
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
+ 
+  var rLine = "";
+  for (i=0; i<pHeight; i++);
+    rLine += "<p>";
+  
+  for (j=0; j<=i; j++){
+    if (j%2)
+      rLine += "<span style = 'color:" + pColorEven + ";'>" + pSymbol + "<span>";
+    else
+      rLine += "<span style = 'color:" + pColorOdd + ";'>" + pSymbol + "<span>";
+   }
+  rLine += "<p>";
+  // console.log (rLine);
+  }
+  document.getElementbyId("upRight").innerHTML = rLine;
+}
+
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+
+  var rLine = "";
+  for (i=pHeight; i>0; i--);
+    rLine += "<p>";
+  
+  for (j=0;  j<i; j++){
+    if (j%2)
+       rLine += "<span style = 'color:" + pColorEven + ";'>" + pSymbol + "<span>";
+    else
+      rLine += "<span style = 'color:" + pColorOdd + ";'>" + pSymbol + "<span>";
+   }
+  rLine += "<p>";
+  // console.log (rLine)
+  }
+  document.getElementById("downRight").innerHTML = rLine;
+}
