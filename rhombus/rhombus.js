3646b05b125d661e0c992a5738dfea6cee82f393
ND02 <97234943+ND02@users.noreply.github.com>
2022-02-14 18:30:10 -0800

    add upLeft

--- rhombus/rhombus.js
+++ rhombus/rhombus.js
@@ -4,8 +4,34 @@
 // var symbol = "*";
 
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol){
+  upLeft (pHeight, pColorEven, pColorOdd, pSymbol);
   upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+  downLeft (pHeight, pColorEven, pColorOdd, pSymbol);
   downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+}
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+ 
+  var rLine = "";
+  for (i=1; i<pHeight; i++);
+    rLine += "<p>";
+  
+  //Fill left side
+  for (x=1; x<=pHeight-i; x++){
+    //rLine += "-";
+    rLine += "<span class='space'>" + pSymbol + "<span>";
+  }
+  
+  for (j=x; j<=pHeight; j++){
+    if (j%2)
+      rLine += "<span style = 'color:" + pColorEven + ";'>" + pSymbol + "<span>";
+    else
+      rLine += "<span style = 'color:" + pColorOdd + ";'>" + pSymbol + "<span>";
+   }
+  rLine += "<p>";
+  // console.log (rLine);
+  }
+  document.getElementbyId("upLeft").innerHTML = rLine;
 }
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
