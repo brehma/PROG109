d01d56af4b0d27fb4e3a28f7c982717c3975ea5e
ND02 <97234943+ND02@users.noreply.github.com>
2022-02-19 09:22:48 -0800

    remove individual functions

--- hotel/hotel.js
+++ hotel/hotel.js
@@ -1,43 +1,17 @@
 // Create the template for objects that are hotels
-//function Hotel(name, rooms, booked) {
-  //this.name = name;
-  //this.rooms = rooms;
-  //this.booked = booked;
-  //this.checkAvailability = function() {
-    //return this.rooms - this.booked;
-  //};
-//}
+function Hotel(name, rooms, booked) {
+  this.name = name;
+  this.rooms = rooms;
+  this.booked = booked;
+  this.checkAvailability = function() {
+    return this.rooms - this.booked;
+  };
+}
 
 // Create two hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
-  function quayHotel(Quay, 40, 25) {
-    this.name = Quay;
-    this.rooms = 40;
-    this.booked = 25;
-    this.checkAvailability = function() {
-      return this.rooms - this.booked;
-    };
-  }
-
 var parkHotel = new Hotel('Park', 120, 77);
-  function Hotel(Park, 120, 77) {
-    this.name = Park;
-    this.rooms = 120;
-    this.booked = 77;
-    this.checkAvailability = function() {
-      return this.rooms - this.booked;
-    };
-  }
-
 var sunsetHotel = new Hotel ('Sunset', 86, 10);
-  function Hotel(Sunset, 86, 10) {
-    this.name = Sunset;
-    this.rooms = 86;
-    this.booked = 10;
-    this.checkAvailability = function() {
-      return this.rooms - this.booked;
-    };
-  }
 
 // Update the HTML for the page
 var details1 = quayHotel.name + ' rooms: ';
